006ef1c13946efc3fd0c9c017c1a530f9c8240f0
Korolev3377 <pingdowner@gmail.com>
2023-06-14 13:42:21 +0400

    db done

--- commands/database/dbcontrol.py
+++ commands/database/dbcontrol.py
@@ -1,17 +1,69 @@
-def connect(): pass
+import sqlite3 as sql
 
 
-def disconnect(): pass
+class DataBase:
+    def __init__(self):
+        self.db_connection = None
+        self.db_cursor = None
+
+    def connect(self):
+        self.db_connection = sql.connect("database.db")
+        self.db_cursor = self.db_connection.cursor()
+
+    def disconnect(self):
+        self.db_connection.commit()
+        self.db_connection.close()
+
+    def get_user_info(self, user_id: int, user_name: str, user_language: str) -> dict:
+        retry = 1
+        while retry > 0:
+            self.db_cursor.execute(f"""
+            SELECT id,
+            name,
+            wealth,
+            score,
+            language
+            FROM users
+            WHERE id = {user_id};""")
+            i = self.db_cursor.fetchone()
+            if i:
+                data = {
+                    "id": i[0],
+                    "name": i[1],
+                    "wealth": i[2],
+                    "score": i[3],
+                    "language": i[4],
+                }
+                return data
+            else:
+                self.db_cursor.execute(f"""
+                INSERT INTO users (
+                id,
+                name,
+                wealth,
+                score,
+                language
+                )
+                VALUES (
+                {user_id},
+                {user_name},
+                0,
+                0,
+                {user_language}
+                );""")
+                retry -= 1
+
+    def ch_user_money(self, users: list, mode: str, value: int) -> None:
+        if mode == "add":
+            pass
+        elif mode == "sub":
+            pass
+        elif mode == "set":
+            pass
+        elif mode == "move" and len(users) == 2:
+            pass
+        else:
+            return None
 
 
-def add_user_money(): pass
-
-
-def sub_user_money(): pass
-
-
-def set_user_money(): pass
-
-
-def transaction_between_users(): pass
-
+DB = DataBase()
--- commands/database/main.py
+++ commands/database/main.py
@@ -4,11 +4,13 @@ import asyncio
 
 from discord import app_commands
 from discord.app_commands import locale_str as _ls
+from .dbcontrol import DB
 
 from translator.main import T
 from environment.variable import *
 
 _ = ""
+GETBALANCE = "getbalance"
 WEALTH_GRP_NAME = "wgrpn"
 WEALTH_GRP_DESK = "wgrpd"
 BALANCE_NAME = "wbalancen"
@@ -17,6 +19,10 @@ BALANCE_DESC = "wbalanced"
 _locale = {
     _: {EN: "",
         RU: ""},
+
+    GETBALANCE: {EN: "You have {_} lots.",
+                 RU: "У тебя есть {_} лот."},
+
     WEALTH_GRP_NAME: {EN: "wallet",
                       RU: "кошелек"},
     WEALTH_GRP_DESK: {EN: "You control your money",
@@ -42,9 +48,18 @@ async def balancecmd(interaction: discord.Interaction):
     while LOCK.locked():
         await asyncio.sleep(1)
     async with LOCK:
+        DB.connect()
+        data = DB.get_user_info(interaction.user.id, interaction.user.name, interaction.locale)
+        DB.disconnect()
         _T.set_string(
             string=_ls(
-                "Ты нищий"
+                GETBALANCE,
+                extras={
+                    FORMAT: {
+                        "_": _T.stranslate(st=_ls(data.get("wealth")))
+                    }
+                }
             )
         )
         await interaction.followup.send(_T.stranslate())
+

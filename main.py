172ad9dda0f1f3eff8b4d6be74d3982eeb3fda52
Korolev3377 <pingdowner@gmail.com>
2023-07-28 16:40:39 +0400

    on_error ephemeral on

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         _T.set_string(
             _ls("error")
         )
-        await interaction.followup.send(_T.stranslate(), ephemeral=False)
+        await interaction.followup.send(_T.stranslate(), ephemeral=True)
         return False
 
         # Системные комманды могут вызываться без последствий

999e1a4cfd2c5bf398c610ab9325361a7c6c833d
Korolev3377 <pingdowner@gmail.com>
2026-05-15 10:40:19 +0400

    test menu

--- satbot/commands/bot_settings.py
+++ satbot/commands/bot_settings.py
@@ -4,6 +4,8 @@ import logging
 # ----- Discord Python Library ----- #
 import discord
 
+from satbot.menu import Menu
+
 Log = logging.getLogger(__name__)
 
 @discord.app_commands.command(
@@ -12,8 +14,19 @@ Log = logging.getLogger(__name__)
 )
 @discord.app_commands.checks.has_permissions(administrator=True)
 async def command(interaction: discord.Interaction):
-    await interaction.response.defer(thinking=True, ephemeral=True)
-    await interaction.followup.send("WIP")
+    await interaction.response.defer(thinking=True)
+    opening_lines = ["-# Это тестовая версия меню", "# Меню"]
+    main_menu_name = "## Главное меню"
+    items_map = {
+        "Вложенное меню": {},
+        "Переключатель 1": True,
+        "Переключатель 2": False,
+        "Текстовое поле": "Hello, World!",
+        "Просто текст": None,
+
+    }
+    menu = Menu(opening_lines, main_menu_name, items_map)
+    await interaction.followup.send(menu.render())
 
 @command.error
 async def error(interaction: discord.Interaction, e: discord.app_commands.AppCommandError):
